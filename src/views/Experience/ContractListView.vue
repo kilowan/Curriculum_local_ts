3839781ec9a7decd2d6dbf0093b560e25232c4d0
PC-GAMER\Juan <kilowan2@gmail.com>
2022-10-24 18:12:29 +0200

    fix issues

--- src/views/Experience/ContractListView.vue
+++ src/views/Experience/ContractListView.vue
@@ -72,19 +72,19 @@ export default {
       projectData: "",
     };
   },
-  update(contract: Contract) {
-    var filtered = this.contractsData.filter(
-      (data: any) => data.id !== contract.id
-    );
-    filtered.push(contract);
-    this.contractsData = filtered;
-    this.$emit("update", this.contractsData);
-  },
   methods: {
-    splice(index: number) {
-      this.contractsData = this.contractsData.filter(
-        (data: any) => data.id !== index
+      splice(index: number) {
+        this.contractsData = this.contractsData.filter(
+          (data: any) => data.id !== index
+        );
+        this.$emit("update", this.contractsData);
+      },
+      update(contract: any) {
+      var filtered = this.contractsData.filter(
+        (data: any) => data.id !== contract.id
       );
+      filtered.push(contract);
+      this.contractsData = filtered;
       this.$emit("update", this.contractsData);
     },
   },
