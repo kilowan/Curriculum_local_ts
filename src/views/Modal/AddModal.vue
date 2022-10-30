4ed6c4446efdea8e2247bef09aff7120a7ddc893
PC-GAMER\Juan <kilowan2@gmail.com>
2022-10-30 16:50:51 +0100

    refactor modals

---
+++ src/views/Modal/AddModal.vue
@@ -0,0 +1,71 @@
+<template>
+    <b-modal
+    :id="`add-${modalId}`"
+    :title="`Añadir ${modalTitle}`"
+    ok-title="Guardar"
+    @ok="save"
+    @cancel="cancel"
+  >
+    <label>Nombre</label> <input type="text" v-model="newComponent.name" /> <br />
+    <label v-if="componentDatatype === 'Language'">Nivel</label> 
+    <input v-if="componentDatatype === 'Language'" type="text" v-model="newComponent.level" /> <br />
+    <label v-if="componentDatatype === 'Academic' || componentDatatype === 'Experience'">Centro/Lugar:</label>
+    <input v-if="componentDatatype === 'Academic' || componentDatatype === 'Experience'" type="text" v-model="newComponent.place" /> <br />
+    <label v-if="componentDatatype === 'Experience'">Fecha de inicio</label>
+    <input v-if="componentDatatype === 'Experience'" type="date" v-model="newComponent.initDate" min="2015-01-01" max="2030-12-31" />
+    <br />
+    <label v-if="componentDatatype === 'Experience'">Fecha de fin</label>
+    <input
+      v-if="componentDatatype === 'Experience'"
+      type="date"
+      v-model="newComponent.finishDate"
+      min="2015-01-01"
+      max="2030-12-31"
+    />
+    <label v-if="componentDatatype === 'Academic'">Graduación</label>
+    <input
+      v-if="componentDatatype === 'Academic'"
+      type="date"
+      v-model="newComponent.graduationDate"
+      min="2015-01-01"
+      max="2030-12-31"
+    />
+    <br />
+  </b-modal>
+</template>
+
+<script lang="ts">
+import { Component } from "@/Config/types";
+
+export default {
+  name: "AddModal",
+  props: {
+    modalTitle: {
+      type: String,
+      required: true,
+    },
+    modalId: {
+      type: String,
+      required: true,
+    },
+    componentDatatype: {
+      type: String,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      newComponent: {} as Component
+    };
+  },
+  methods: {
+    cancel() {
+      this.newComponent = {} as Component;
+    },
+    save() {
+      this.newComponent.componentDataType = this.componentDatatype;
+      this.$emit('save', this.newComponent);
+    }
+  }
+};
+</script>
---
+++ src/views/Modal/DeleteModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-modal
+    :id="`delete-${modalId}-${componentData.id}`"
+    :title="`Eliminar ${modalTitle}`"
+    ok-title="Eliminar"
+    @ok="$emit('remove', componentData)"
+  >
+    <div style="text-align: center; margin: 0 auto; width: 380px">
+      <h1>¿Seguro que quieres eliminar {{ message }} {{ componentData.name }}?</h1>
+    </div>
+  </b-modal>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: "DeleteModal",
+  props: {
+    componentData: {
+      type: Object,
+      required: true,
+    },
+    modalId: {
+      type: String,
+      required: true
+    },
+    modalTitle: {
+      type: String,
+      required: true
+    },
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {};
+  },
+};
+</script>
