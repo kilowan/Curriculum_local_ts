f9742c736586467a8850cb039548510a9f5bb098
PC-GAMER\Juan <kilowan2@gmail.com>
2022-10-30 16:50:01 +0100

    refactor types

--- src/Config/types.ts
+++ src/Config/types.ts
@@ -1,83 +1,44 @@
-export interface base {
+export interface CurriculumDetail {
   id: number;
-}
-export interface CurriculumDetail extends base {
   fullName: string;
   description: string;
   phoneNumber: string;
   email: string;
-  experience: Array<Experience>;
-  otherTraining: Array<Training>;
-  academicTraining: Array<Academic>;
-  languageList: Array<Language>;
-  socialMedia: Array<SocialMedia>;
-  otherData: Array<OtherData>;
+  experience: Array<Component>;
+  skillList: Array<Component>;
+  academicTraining: Array<Component>;
+  languageList: Array<Component>;
+  socialMedia: Array<Component>;
+  otherData: Array<Component>;
 }
-export interface Experience extends base {
+export interface Component {
+  id: number;
   name: string;
-  type: number;
-  place: string;
-  initDate: string;
-  finishDate: string;
-  contracts: Array<Contract>;
-}
-export interface Contract extends base {
-  name: string;
-  projects: Array<Project>;
-}
-export interface Project extends base {
-  name: string;
-  descriptionList: Array<Description>;
-}
-export interface Description extends base {
-  name: string;
-}
-export interface Training extends base {
-  name: string;
-  contents: Array<Content>;
-}
-export interface Academic extends Training {
-  place: string;
+  place?: string;
   initDate?: string;
   finishDate?: string;
-  graduationDate: string;
-}
-export interface Content extends base {
-  name: string;
-  subContents: Array<SubContent>;
-}
-export interface SubContent extends base {
-  name: string;
-}
-export interface Language extends base {
-  name: string;
-  level: string;
-}
-export interface SocialMedia extends base {
-  name: string;
-  type: number;
-}
-export interface OtherData extends base {
-  name: string;
-  values: Array<Value>;
-}
-export interface Value extends base {
-  name: string;
-}
-export enum ContentType {
-  Academic,
-  Complementary,
+  graduationDate?: string;
+  childrensTitle?: string;
+  level?: string;
+  type?: number;
+  childrens?: Array<Component>;
+  componentDataType?: string;
 }
 export enum SocialMediaType {
   Linkedin = 1,
   Infojobs = 2,
   GitHub = 3,
 }
-export enum ExperienceType {
-  personal = 1,
-  professional = 2,
-}
-export enum TrainingType {
-  academic = 1,
-  other = 2,
+export enum ComponentType {
+  Experience = 1,
+  Academic = 2,
+  Skills = 3,
+  Languages = 4,
+  Other = 5,
+  Content = 6,
+  SubContent = 7,
+  Contract = 8,
+  Project = 9,
+  Description = 10,
+  Value = 11
 }
